3b1c8318f7cb6d531e0ec73e4245dec4007a74f9
kungru <108171226+kungru@users.noreply.github.com>
2022-10-09 20:36:51 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,64 @@
 
+// import Header from './Page/Header/Header';
+import Example from './Page/Body/Carousel_zx';
+import Header from './Page/Header/Header';
 import Footer from './Page/Footer/Footer';
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import Container_card from './Page/Body/Container_card';
 function App() {
+  const [cards, setCards] = useState([]);
+
+  useEffect(
+    () => {
+      fetch('https://634015dae44b83bc73c898c3.mockapi.io/api/v1/card')
+        .then((res) => {
+          return (res.json())
+        }).then((data) => {
+          console.log(data);
+          setCards(data);
+          console.log(cards);
+        })
+    }, []
+
+  )
+
   return (
-<div className='App'>
-    <Footer/>
-</div>
+    <div className='App'>
+
+      <Container fluid className="bg-light border" style={{ backgroundColor: 'white', width: '100%', marginLeft: '0', marginRight: '0' }}>
+        <Row>
+          <Example/>
+        </Row>
+        <Row>
+          <Col xs={3} id="sidebar-wrapper">
+            <Header />
+          </Col>
+          <Col xs={9} id="page-content-wrapper">
+            <div className='container_card'>
+              {cards.map((item) => {
+                return (
+                  <Container_card
+                    name={item.name}
+                    style={item.style}
+                    shape={item.shape}
+                    url={item.url}
+                    price={item.price}
+                  />
+                )
+              })}
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Footer />
+          </Col>
+        </Row>
+      </Container>
+
+      {/* <Footer/> */}
+    </div>
   );
 }
 
---
+++ src/Page/Body/Carousel_zx.js
@@ -0,0 +1,101 @@
+import React, { useState } from 'react';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption,
+} from 'reactstrap';
+import './Carousel_zx.css';
+const items = [
+  {
+    src: './images/3.jpg',
+    objectfit:'cover',
+    // objectpos:'0% 0%',
+    altText: 'Slide 1',
+    caption: 'Slide 1',
+    key: 1,
+  },
+  {
+    src: 'https://img5.goodfon.com/wallpaper/nbig/e/e3/devushka-portret-litso-vzgliad-kai-boet-kai-bottcher-ochki-o.jpg',
+    altText: 'Slide 2',
+    caption: 'Slide 2',
+    key: 2,
+    objectfit:'cover',
+    objectpos:'0% 30%',
+  },
+  {
+    src: 'https://img5.goodfon.com/wallpaper/nbig/b/47/girl-glasses-blue.jpg',
+    altText: 'Slide 3',
+    caption: 'Slide 3',
+    key: 3,
+    objectfit:'cover',
+    objectpos:'0% 30%',
+  },
+];
+
+function Example(args) {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  };
+
+  const slides = items.map((item) => {
+    return (
+      <CarouselItem
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+        key={item.src}
+      >
+        <img src={item.src} alt={item.altText} style={{objectFit:`${item.objectfit}`, objectPosition:`${item.objectpos}`}}/>
+        <CarouselCaption
+          captionText={item.caption}
+          captionHeader={item.caption}
+        />
+      </CarouselItem>
+    );
+  });
+
+  return (
+    <Carousel
+      activeIndex={activeIndex}
+      next={next}
+      previous={previous}
+      {...args}
+    >
+      <CarouselIndicators
+        items={items}
+        activeIndex={activeIndex}
+        onClickHandler={goToIndex}
+      />
+      {slides}
+      <CarouselControl
+        direction="prev"
+        directionText="Previous"
+        onClickHandler={previous}
+      />
+      <CarouselControl
+        direction="next"
+        directionText="Next"
+        onClickHandler={next}
+      />
+    </Carousel>
+  );
+}
+
+export default Example;
